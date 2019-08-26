ad2c81622d3bd5e616b25186bbb7b0fc437406d5
marcus290 <marcusmchan@gmail.com>
2019-08-26 12:44:51 +1200

    add package declarations

--- c/TCPClient.java
+++ c/TCPClient.java
@@ -1,5 +1,6 @@
+package c;
 /**
- * Code is taken from Computer Networking: A Top-Down Approach Featuring 
+ * Code is modified from Computer Networking: A Top-Down Approach Featuring 
  * the Internet, second edition, copyright 1996-2002 J.F Kurose and K.W. Ross, 
  * All Rights Reserved.
  **/
--- s/Users.java
+++ s/Users.java
@@ -1,3 +1,5 @@
+package s;
+
 import java.io.*; 
 import java.util.Scanner;
 import java.util.ArrayList;
